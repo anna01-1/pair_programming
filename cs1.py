d5ca2946959cb49e39f488199a40628b0a9d5d39
anna01-1 <70250398+anna01-1@users.noreply.github.com>
2024-08-19 16:26:49 +0800

    Update cs1.py

--- cs1.py
+++ cs1.py
@@ -30,7 +30,7 @@ class fileTest(unittest.TestCase):
 
         print('test_jg:')
 
-        rv = testfile.szys()
+        rv = testfile.arithmetic()
 
         assert  0 <= rv <= 100    #测试题目结果是否在0到100以内
 
@@ -39,7 +39,7 @@ class fileTest(unittest.TestCase):
 
         print('test_zs:')
 
-        rv = testfile.szys()
+        rv = testfile.arithmetic()
 
         self.assertIsInstance(rv,int)  #测试题目答案是否是整数
 
@@ -48,7 +48,7 @@ class fileTest(unittest.TestCase):
 
         print('test_k:')
 
-        rv = testfile.szys()
+        rv = testfile.arithmetic()
 
         self.assertIsNotNone(rv)    #测试函数返回值是否为空
 
@@ -57,7 +57,7 @@ class fileTest(unittest.TestCase):
 
         print('test_n:')
 
-        rv = testfile.szys()
+        rv = testfile.arithmetic()
 
         self.assertEqual(rv,10)     #测试函数返回值为具体某数，因为题目是随机产生，所以本条测试在大部分测试下都应不通过
 
